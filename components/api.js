85e8138d529fdf09c2860ee42ff11b392465084e
Uyi <ihazauyi@gmail.com>
2022-05-21 20:50:18 +0100

    feat: Implement the error messages

--- components/api.js
+++ components/api.js
@@ -1,7 +1,18 @@
 const Translator = require("./translator.js");
+const translator = new Translator();
+const VALID_LOCALES = ["american-to-british", "british-to-american"];
 
 const translate = (req, res) => {
-  const translator = new Translator();
+  const { text, locale } = req.body;
+  if (text === undefined || locale === undefined) {
+    throw new Error("Required field(s) missing");
+  }
+  if (text.length === 0) {
+    throw new Error("No text to translate");
+  }
+  if (VALID_LOCALES.indexOf(locale) < 0) {
+    throw new Error("Invalid value for locale field");
+  }
   res.json({ ...req.body });
 };
 module.exports = translate;
