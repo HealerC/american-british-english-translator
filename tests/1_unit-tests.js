93368d72d126b7fde4cc32594ee9c0dfdc949a37
Uyi <ihazauyi@gmail.com>
2022-05-24 15:06:28 +0100

    test: Add unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,33 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
+const translator = new Translator();
 
-suite('Unit Tests', () => {
+const samples = require("./sample.js");
+const locales = {
+  AM_BR: "american-to-british",
+  BR_AM: "british-to-american",
+};
 
+suite("Unit Tests", () => {
+  suite("American to British", () => {
+    run(samples, locales.AM_BR);
+  });
+  suite("British to American", () => {
+    run(samples, locales.BR_AM);
+  });
 });
+
+/* Generically test translation based on locale given */
+function run(samples, locale) {
+  samples.forEach((sentence) => {
+    if (sentence.locale === locale) {
+      let text = sentence.text;
+      let expected = sentence.translation;
+      test(text, () => {
+        assert.equal(translator.translate(text, locale), expected);
+      });
+    }
+  });
+}
