687af7372e3fcabb6475e38fa5554c2d594f5eea
Uyi <ihazauyi@gmail.com>
2022-05-24 14:04:21 +0100

    feat: Add sample sentences with their correct translations

---
+++ tests/sample.js
@@ -0,0 +1,116 @@
+module.exports = [
+  {
+    text: "Mangoes are my favorite fruit.",
+    translation:
+      'Mangoes are my <span class="highlight">favourite</span> fruit.',
+    locale: "american-to-british",
+  },
+  {
+    text: "I ate yogurt for breakfast.",
+    translation: 'I ate <span class="highlight">yoghurt</span> for breakfast.',
+    locale: "american-to-british",
+  },
+  {
+    text: "We had a party at my friend's condo.",
+    translation:
+      'We had a party at my friend\'s <span class="highlight">flat</span>.',
+    locale: "american-to-british",
+  },
+  {
+    text: "Can you toss this in the trashcan for me?",
+    translation:
+      'Can you toss this in the <span class="highlight">bin</span> for me?',
+    locale: "american-to-british",
+  },
+  {
+    text: "The parking lot was full.",
+    translation: 'The <span class="highlight">car park</span> was full.',
+    locale: "american-to-british",
+  },
+  {
+    text: "Like a high tech Rube Goldberg machine.",
+    translation:
+      'Like a high tech <span class="highlight">Heath Robinson device</span>.',
+    locale: "american-to-british",
+  },
+  {
+    text: "To play hooky means to skip class or work.",
+    translation:
+      'To <span class="highlight">bunk off</span> means to skip class or work.',
+    locale: "american-to-british",
+  },
+  {
+    text: "No Mr. Bond, I expect you to die.",
+    translation:
+      'No <span class="highlight">Mr</span> Bond, I expect you to die.',
+    locale: "american-to-british",
+  },
+  {
+    text: "Dr. Grosh will see you now.",
+    translation: '<span class="highlight">Dr</span> Grosh will see you now.',
+    locale: "american-to-british",
+  },
+  {
+    text: "Lunch is at 12:15 today.",
+    translation: 'Lunch is at <span class="highlight">12.15</span> today.',
+    locale: "american-to-british",
+  },
+  {
+    text: "We watched the footie match for a while.",
+    translation:
+      'We watched the <span class="highlight">soccer</span> match for a while.',
+    locale: "british-to-american",
+  },
+  {
+    text: "Paracetamol takes up to an hour to work.",
+    translation:
+      '<span class="highlight">Tylenol</span> takes up to an hour to work.',
+    locale: "british-to-american",
+  },
+  {
+    text: "First, caramelise the onions.",
+    translation: 'First, <span class="highlight">caramelize</span> the onions.',
+    locale: "british-to-american",
+  },
+  {
+    text: "I spent the bank holiday at the funfair.",
+    translation:
+      'I spent the <span class="highlight">public holiday</span> at the <span class="highlight">carnival</span>.',
+    locale: "british-to-american",
+  },
+  {
+    text: "I had a bicky then went to the chippy.",
+    translation:
+      'I had a <span class="highlight">cookie</span> then went to the <span class="highlight">fish-and-chip shop</span>.',
+    locale: "british-to-american",
+  },
+  {
+    text: "I've just got bits and bobs in my bum bag.",
+    translation:
+      'I\'ve just got <span class="highlight">odds and ends</span> in my <span class="highlight">fanny pack</span>.',
+    locale: "british-to-american",
+  },
+  {
+    text: "The car boot sale at Boxted Airfield was called off.",
+    translation:
+      'The <span class="highlight">swap meet</span> at Boxted Airfield was called off.',
+    locale: "british-to-american",
+  },
+  {
+    text: "Have you met Mrs Kalyani?",
+    translation: 'Have you met <span class="highlight">Mrs.</span> Kalyani?',
+    locale: "british-to-american",
+  },
+  {
+    text: "Prof Joyner of King's College, London.",
+    translation:
+      '<span class="highlight">Prof.</span> Joyner of King\'s College, London.',
+    locale: "british-to-american",
+  },
+  {
+    text: "Tea time is usually around 4 or 4.30.",
+    translation:
+      'Tea time is usually around 4 or <span class="highlight">4:30</span>.',
+    locale: "british-to-american",
+  },
+];
